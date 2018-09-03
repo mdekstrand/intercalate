eea803cb6bc272c308915c74f977f494ade9372a
Michael Ekstrand <md@ekstrandom.net>
2018-09-03 16:36:45 -0600

    initial commit of working intercalate

---
+++ intercalate.js
@@ -0,0 +1,21 @@
+function intercalate(arr, sep, final, pair) {
+  if (arr.length < 2) {
+    return arr;
+  } else if (arr.length && pair !== undefined) {
+    let [e1, e2] = arr;
+    return [e1, pair, e2];
+  } else {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (final && i == arr.length - 1) {
+        res.push(final)
+      } else if (i > 0) {
+        res.push(sep)
+      }
+      res.push(arr[i]);
+    }
+    return res;
+  }
+}
+
+module.exports = intercalate;
---
+++ test/intercalate.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+
+const intercalate = require('../intercalate');
+
+describe('intercalate', () => {
+  it('should return an empty array for empty input', () => {
+    let res = intercalate([], ' ');
+    assert.equal(res.length, 0);
+  });
+
+  it('should return a single-element array unchanged', () => {
+    let res = intercalate(['bob'], ' ');
+    assert.equal(res.length, 1);
+    assert.equal(res[0], 'bob');
+  });
+
+  it('should intersperse the joiner in a two-element array', () => {
+    let res = intercalate(['wumpus', 'splintercat'], ' ');
+    assert.deepEqual(res, ['wumpus', ' ', 'splintercat']);
+  });
+
+  it('should intersperse the joiner in a three-element array', () => {
+    let res = intercalate(['wumpus', 'splintercat', 'woozle'], true);
+    assert.deepEqual(res, ['wumpus', true, 'splintercat', true, 'woozle']);
+  });
+
+  it('should use the terminating joiner when given', () => {
+    let res = intercalate(['a', 'b', 'c'], 2, 5);
+    assert.deepEqual(res, ['a', 2, 'b', 5, 'c']);
+  });
+
+  it('should use the terminating joiner for many items', () => {
+    let res = intercalate(['a', 'b', 'c', 'd', 'e'], 2, 5);
+    assert.deepEqual(res, ['a', 2, 'b', 2, 'c', 2, 'd', 5, 'e']);
+  });
+
+  it('should use the terminating joiner in a two-element array', () => {
+    let res = intercalate(['wumpus', 'splintercat'], ' ', 'x');
+    assert.deepEqual(res, ['wumpus', 'x', 'splintercat']);
+  });
+
+  it('should use the two-element joiner in a two-element array', () => {
+    let res = intercalate(['wumpus', 'splintercat'], ' ', 'x', 'two');
+    assert.deepEqual(res, ['wumpus', 'two', 'splintercat']);
+  });
+});
